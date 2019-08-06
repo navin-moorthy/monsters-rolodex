bdc2f6ff354686b6ae678b89f5464d419ac8279a
NavinNavi19 <navin007.a@gmail.com>
2019-08-06 20:34:53 +0000

    Components Tree Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
+
+// Components
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -20,9 +22,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
+        <CardList>
+          {this.state.monsters.map(monster => {
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })}
+        </CardList>
       </div>
     );
   }

828bb9f89e6c813b0295eafd6de779b281f1d69e
NavinNavi19 <navin007.a@gmail.com>
2019-08-08 07:36:47 +0000

    Updates

---
+++ precache-manifest.d763db570c34b2e21e38e635564802f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0087139cbdd966cecc6cb690e7f94b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "379a7be0f68baa8c6e34",
+    "url": "/monsters-rolodex/static/css/main.cd476039.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "379a7be0f68baa8c6e34",
+    "url": "/monsters-rolodex/static/js/main.a7f1d969.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
